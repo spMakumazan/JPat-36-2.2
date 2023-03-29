992f11bf150885288617536b0187a5c2dd489ced
spMakumazan <sp.makumazan@gmail.com>
2023-03-29 21:24:54 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("11110000", "111"));
+        System.out.println(bins.mult("11", "10"));
+    }
+}
